7d166184d140cc82936736e669a93c56a6d5114d
phagenlocher <philipp.hagenlocher@tum.de>
2019-05-30 14:48:32 +0200

    Fixed test script

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
-formula=`./mochi.native -r 3`
+if [ -z "$1" ]
+then
+	echo "Usage: ./test.sh <number of recursive ltl depth>"
+fi
 
 while [ $? -eq 0 ]
 do
+formula=`./mochi.native -r $1`
 ltlcross -f "$formula" 'ltl2tgba -s %s >%O' './mochi.native -f %f >%O'
 done
 
